9873cf2e932e5cb3057461959371faf0a15d23b3
hariharan872005 <hariharan11ahmhss@gmail.com>
2025-07-25 22:49:44 +0530

    Final fix for settings.py merge conflict

--- Backend/Project_1/Pos_backend_Sample/settings.py
+++ Backend/Project_1/Pos_backend_Sample/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'corsheaders'
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
